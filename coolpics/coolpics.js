0cfbcd14824410c8f0c6f5bc36b6f0b7ea057513
matthew-m-morton <mor21018@byui.edu>
2024-05-18 22:08:48 -0600

    week 4

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -30,9 +30,9 @@ function viewerTemplate(pic, alt) {
 	// create a variable to hold the element that was clicked on from event.target
     const clickedElement = event.target;
 	// get the src attribute from that element and 'split' it on the "-"
-    const srcParts = clickedElement.src.split('-');
+    const srcParts = clickedElement.src.split('_');
 	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
-    const newSrc = srcParts[0] + '-full.jpeg';
+    const newSrc = srcParts[0] + '_full.jpeg';
 	// insert the viewerTemplate into the top of the body element
 	// (element.insertAdjacentHTML("afterbegin", htmltoinsert))
     const viewerHTML = viewerTemplate(newSrc, clickedElement.alt);
