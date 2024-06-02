5f9efc7224daa46e328f1b2704ff942b3b1a14a5
matthew-m-morton <mor21018@byui.edu>
2024-06-01 19:22:21 -0600

    week 6

--- blog/blog.js
+++ blog/blog.js
@@ -23,5 +23,71 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his \"Aunt Pol\" and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
 	}
 ]
+
+function articleTemplate(id) {
+    // retrieve article with id
+    const article = articles.find(article => article.id === id);
+
+    // Return the HTML template as a string
+    return `
+        <div class="article" id="${article.id}">
+            <div class="details">
+                <p class="date">${article.date}</p>
+                <p class="agerange">${article.ages}</p>
+                <p class="genre">${article.genre}</p>
+                <p class="rating">${article.stars}</p>
+            </div>
+            <div class="bookdescription">
+                <h2 class="booktitle">${article.title}</h2>
+                <img src="${article.imgSrc}" alt="${article.imgAlt}">
+                <p class="blurb">${article.description}</p>
+            </div>
+        </div>`;
+}
+
+// Function to add the articles
+function renderArticles() {
+    const articleList = document.getElementById('articlelist');
+    
+    // Clear articles
+    articleList.innerHTML = '';
+
+    // create each article in array
+    articles.forEach(article => {
+        articleList.innerHTML += articleTemplate(article.id);
+    });
+}
+
+renderArticles();
+
+
+
+// article template
+/* <div class="article" id="">
+    <div class="details">
+        <p class="date"></p>
+        <p class="agerange"></p>
+        <p class="genre"></p>
+        <p class="rating"></p>
+    </div>
+    <div class="bookdescription">
+        <h2 class="booktitle"></h2>
+        <img src="" alt="">
+        <p class="blurb"></p>
+    </div>
+</div> */
